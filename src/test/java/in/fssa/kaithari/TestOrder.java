0159d5c1faddabed238162b54c1045179a71bd79
vasumeenu004 <116268882+vasumeenu004@users.noreply.github.com>
2023-09-25 17:28:07 +0530

    small changes

--- src/test/java/in/fssa/kaithari/TestOrder.java
+++ src/test/java/in/fssa/kaithari/TestOrder.java
@@ -1,5 +1,9 @@
 package in.fssa.kaithari;
 
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+
+import in.fssa.kaithari.service.OrderService;
+
 public class TestOrder {
 
 }
--- src/test/java/in/fssa/kaithari/TestUpdateSeller.java
+++ src/test/java/in/fssa/kaithari/TestUpdateSeller.java
@@ -4,24 +4,24 @@ import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
 import org.junit.jupiter.api.Test;
 
-import in.fssa.kaithari.model.Seller;
 import in.fssa.kaithari.service.SellerService;
 
 public class TestUpdateSeller {
-	@Test
-	void testUpdateSeller() {
-	    SellerService sellerService = new SellerService();
+    @Test
+    void testUpdateSeller() {
+        SellerService sellerService = new SellerService();
 
-	    Seller updateSeller = new Seller();
-	    updateSeller.setId(3);
-	    updateSeller.setName("New Seller Name");
-	    updateSeller.setEmail("newemail@example.com"); // Update other fields as needed
+        int id = 3;
+        String newName = "New Seller Name";
+        String newAddress = "New Address";
+        String newDistrict = "New District";
+        long newMobileNumber = 1234567890L;
+        int newPincode = 12345; 
+        String newVillage = "New Village";
 
-	    System.out.println(updateSeller.toString());
-
-	    assertDoesNotThrow(() -> {
-	        sellerService.update(updateSeller);
-	    });
-	}
-
+        assertDoesNotThrow(() -> {
+            sellerService.updateAddress(id, newName, newAddress, newDistrict, newMobileNumber, newPincode, newVillage);
+        });
+    }
 }
+
